4f117a68aeb6933370a239cea21525eacca7b7e2
Krzysztof Małczak <gajderstudio@gmial.com>
2021-02-16 22:19:44 +0100

    Installed bootstrap dependency

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import Navbar from "./components/navbar";
 
 function App() {
-  return <p>Hello</p>;
+  return <Navbar />;
 }
 
 export default App;
---
+++ src/components/navbar/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+
+export default function Navbar() {
+  return <Button>NAVBAR</Button>;
+}
