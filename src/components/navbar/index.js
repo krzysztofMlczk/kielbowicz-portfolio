49c6578c38c3a1f25391ffd9eb066492170c14e5
Krzysztof Małczak <gajderstudio@gmial.com>
2021-02-17 12:04:03 +0100

    Implemented navbar component

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,6 +1,28 @@
 import React from "react";
-import Button from "react-bootstrap/Button";
+import { Navbar, Nav, Container } from "react-bootstrap";
+import "./styles.css";
 
-export default function Navbar() {
-  return <Button>NAVBAR</Button>;
+function NavigationBar() {
+  return (
+    <Container style={{ maxWidth: "2000px" }}>
+      <Navbar expand="lg">
+        <Navbar.Brand href="#home">
+          <img src="images/logo.png" alt="cannot display" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#about">About</Nav.Link>
+            <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+            <Nav.Link href="#pages">Pages</Nav.Link>
+            <Nav.Link href="#blog">Blog</Nav.Link>
+            <Nav.Link href="#contact">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    </Container>
+  );
 }
+
+export default NavigationBar;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 
+import "./index.css";
 // Importing the Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 
